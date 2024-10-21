74326f4cf572cae3bcedd4928ab678f941006157
awneet23 <awneet23@gmail.com>
2024-10-22 00:09:34 +0530

    initial commit any random message

--- app.js
+++ app.js
@@ -1,30 +1,84 @@
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const path = require("path");
+// const socketio = require("socket.io");
+// const server = http.createServer(app);
+// // const { Server } = require("socket.io");
+
+// const io = socketio(server);
+
+// app.set('view engine', 'ejs');
+// app.use(express.static(path.join(__dirname,'public')))
+// io.on('connection',(socket)=>{
+//   socket.on('send-location',(data)=>{
+//     io.emit('receive-location',{id:socket.id,...data})
+//   })
+
+//   socket.on('disconnect',()=>{
+//     io.emit('user-disconnected',socket.id)
+//   })
+//   console.log('a user')
+// })
+
+// server.listen(3000,()=>{
+//   console.log('listening on port 3000')
+// })
+
+// app.get('/',(req,res)=>{
+//   res.render('index')
+// })
+
+
+
+
+
 const express = require("express");
 const app = express();
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
-const server = http.createServer(app);
-// const { Server } = require("socket.io");
 
+const server = http.createServer(app);
 const io = socketio(server);
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'public')))
-io.on('connection',(socket)=>{
-  socket.on('send-location',(data)=>{
-    io.emit('receive-location',{id:socket.id,...data})
-  })
+app.use(express.static(path.join(__dirname, 'public')));
 
-  socket.on('disconnect',()=>{
-    io.emit('user-disconnected',socket.id)
-  })
-  console.log('a user')
-})
+const users = {};
 
-server.listen(3000,()=>{
-  console.log('listening on port 3000')
-})
+io.on('connection', (socket) => {
+  console.log(`A user connected with id ${socket.id}`);
 
-app.get('/',(req,res)=>{
-  res.render('index')
-})
+  // Add new user to the users object with initial location
+  users[socket.id] = { latitude: 0, longitude: 0 };
+
+  // Emit the current state of all users to the new client
+  io.emit('initial-state', users);
+
+  socket.on('send-location', (data) => {
+    console.log(`Location received from ${socket.id}: `, data);
+
+    // Update the user's location in the users object
+    users[socket.id] = data;
+
+    // Emit the updated location to all clients
+    io.emit('receive-location', { id: socket.id, ...data });
+  });
+
+  socket.on('disconnect', () => {
+    // Remove the user from the users object
+    delete users[socket.id];
+    // Emit the user-disconnected event to all clients
+    io.emit('user-disconnected', socket.id);
+    console.log(`User disconnected with id ${socket.id}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,103 @@
+// const socket = io();
+// console.log('hey');
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.watchPosition((position) => {
+//     const { latitude, longitude } = position.coords; // Fixing the variable name here
+//     socket.emit('send-location', { latitude, longitude }); // Fixed 'longitud' to 'longitude'
+//   }, (error) => {
+//     console.error(error);
+//   },
+//   {
+//     enableHighAccuracy: true,
+//     timeout: 5000,
+//     maximumAge: 0
+//   });
+// }
+
+// const map = L.map('map').setView([0, 0], 16); // Fixed capitalization here
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution: 'openStreetMap'
+// }).addTo(map);
+
+
+// const markers = {};
+
+// socket.on('receive-location',(data)=>{
+//   const {id,latitude,longitude} = data
+//   map.setView([latitude,longitude])
+//   if(markers[id]){
+//     markers[id].setLatLng({latitude,longitude})
+//   }
+//   else{
+//     markers[id]  = L.marker([latitude,longitude]).addTo(map)
+//   }
+// })
+
+// socket.on('user-disconnected',()=>{
+//   if(markers[id]){
+//     map.removeLayer(markers[id])
+//     delete markers[id];
+//   }
+
+// })
+
+
+
+
+
 const socket = io();
-console.log('hey');
+const map = L.map('map').setView([0, 0], 16);
 
-if (navigator.geolocation) {
-  navigator.geolocation.watchPosition((position) => {
-    const { latitude, longitude } = position.coords; // Fixing the variable name here
-    socket.emit('send-location', { latitude, longitude }); // Fixed 'longitud' to 'longitude'
-  }, (error) => {
-    console.error(error);
-  },
-  {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  });
-}
-
-const map = L.map('map').setView([0, 0], 16); // Fixed capitalization here
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'openStreetMap'
 }).addTo(map);
 
-
 const markers = {};
 
-socket.on('receive-location',(data)=>{
-  const {id,latitude,longitude} = data
-  map.setView([latitude,longitude])
-  if(markers[id]){
-    markers[id].setLatLng({latitude,longitude})
-  }
-  else{
-    markers[id]  = L.marker([latitude,longitude]).addTo(map)
-  }
-})
+socket.on('initial-state', (users) => {
+  console.log('Initial state:', users);
+  Object.entries(users).forEach(([id, { latitude, longitude }]) => {
+    if (!markers[id]) {
+      markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+  });
+});
 
-socket.on('user-disconnected',()=>{
-  if(markers[id]){
-    map.removeLayer(markers[id])
-    delete markers[id]
+socket.on('receive-location', ({ id, latitude, longitude }) => {
+  console.log(`Received location for id ${id}: ${latitude}, ${longitude}`);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
   }
+});
 
-})
+socket.on('user-disconnected', (id) => {
+  console.log(`User disconnected: ${id}`);
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log(`Sending location: ${latitude}, ${longitude}`);
+      socket.emit('send-location', { latitude, longitude });
+
+      // Set initial map view to user's location
+      map.setView([latitude, longitude], 16);
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    }
+  );
+}
